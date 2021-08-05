02d18b4f4cc2dd7ec1e6ee04e715e9736991698a
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-05 08:46:34 +0530

    feat:check equal

--- line comparison problem/Program.cs
+++ line comparison problem/Program.cs
@@ -1,22 +1,34 @@
-﻿using System;
+﻿
+using System;
 
-namespace line_comparison_problem
+namespace LineComparisonProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
-            int x1, x2, y1, y2;
-            Console.WriteLine("Enter values x1 and y1");
+            Console.WriteLine(" Welcome to Line Comparison Computation Program on Master Branch ");
+            int x1, x2, y1, y2, a1, a2, b1, b2;
+            Console.WriteLine("Enter values for x1 x2 and y1 y2 for Line 1");
             x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x2 and y2");
             x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("The length of the Line is : " + length);
+            Console.WriteLine("Enter values for x3 x4 and y3  y4 for Line 2");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            a2 = Convert.ToInt32(Console.ReadLine());
+            b1 = Convert.ToInt32(Console.ReadLine());
+            b2 = Convert.ToInt32(Console.ReadLine());
+            double len1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double len2 = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            if (len1.Equals(len2))
+            {
+                Console.WriteLine(" Lines are equal in length. ");
+            }
+            else
+            {
+                Console.WriteLine(" Lines are not equal in length. ");
+            }
         }
     }
 }
-
